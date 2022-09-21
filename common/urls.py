714b29fc7a922374aced1082e754a3148a4f95ce
Shabnum Arif <shabnumarif9@gmail.com>
2022-09-21 10:33:34 +0530

    heroku connection

--- common/urls.py
+++ common/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns=[
-    path('common index',views.index_page),
-    path('seller login',views.seller_login),
-    path('customer login',views.customer_login),
-    path('seller signup',views.seller_signup),
-    path('customer signup',views.customer_signup),
-    path('change password',views.change_password),
+    path('common-index',views.index_page),
+    path('',views.seller_login),
+    path('customer-login',views.customer_login),
+    path('seller-signup',views.seller_signup),
+    path('customer-signup',views.customer_signup),
+    path('change-password',views.change_password),
     path('image',views.image),
 ]
